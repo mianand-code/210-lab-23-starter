93a7b82a74df439bbb8609d759aec99ad66fedfd
Mira Anand <miraanand@comcast.net>
2024-10-22 20:53:49 -0700

    10 minute update - 1

--- Goat.h
+++ Goat.h
@@ -1,18 +1,26 @@
+// COMSC-210 | Lab 23 | Mira Anand
+// Module 9, Lesson: std::list & std::set, Assignment: Goat Manager 3001
+// IDE used: Visual Studio Code for Mac
+
 // Goat.h
+// Header file that contains a complete class for Goat objects
 
 #ifndef GOAT_H
 #define GOAT_H
 #include <iostream>
 using namespace std;
 
-class Goat {
-private:
-    string name;
-    int age;
-    string color;
+class Goat { // creation of Goat Class
+private: // private member variables
+    string name; // to hold goat's name
+    int age; // to hold goat's age
+    string color; // to hold color of goat
+
+// public member functions
 public: 
     Goat()                          { name = ""; age = 0; color = ""; }
     // write three more constructors
+
 
     // setters and getters
     void set_name(string n)         { name = n; };
--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
+// COMSC-210 | Lab 23 | Mira Anand
+// Module 9, Lesson: std::list & std::set, Assignment: Goat Manager 3001
+// IDE used: Visual Studio Code for Mac
+
+#include <cstdlib> // needed to generate a random number
+#include <ctime> // needed to generate a random number
+#include <string>
 #include <iostream>
-#include <fstream>
+#include <fstream> // needed for file operations
 #include <iomanip>
-#include <list>
-#include "Goat.h"
+#include <list> // needed to use std::list
+#include "Goat.h" // references the Goat header file, which contains a complete class for Goat objects
 using namespace std;
 
 const int SZ_NAMES = 200, SZ_COLORS = 25, MAX_AGE = 20;
